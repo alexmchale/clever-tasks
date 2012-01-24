5ac25c8a5620ac7d164d25f67ff25db995d2de84
Alex McHale <alex@anticlever.com>
2012-01-23 19:53:50 -0600

    Initial Commit

---
+++ Regularity/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Regularity
+//
+//  Created by Alex McHale on 1/23/12.
+//  Copyright (c) 2012 Gemini SBS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Regularity/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Regularity
+//
+//  Created by Alex McHale on 1/23/12.
+//  Copyright (c) 2012 Gemini SBS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
