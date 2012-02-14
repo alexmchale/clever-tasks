c9b5b8c8791e174d7cb8d24120c2f07686b2aa51
Alex McHale <alex@anticlever.com>
2012-02-13 20:19:25 -0600

    Refactor the uuid generator.

--- Regularity/NSString+NSStringCategory.m
+++ Regularity/NSString+NSStringCategory.m
@@ -2,12 +2,23 @@
 
 @implementation NSString (NSStringCategory)
 
+// A technique of generating a UUID without needing any funky memory.
 + (NSString *) generateUUID
 {
     CFUUIDRef theUUID = CFUUIDCreate(NULL);
-    CFStringRef string = CFUUIDCreateString(NULL, theUUID);
+    CFUUIDBytes theBytes = CFUUIDGetUUIDBytes(theUUID);
+    NSString *format = @"%02X%02X%02X%02X-%02X%02X-%02X%02X-%02X%02X-%02X%02X%02X%02X%02X%02X";
+    
+    NSString *uuid =
+        [NSString stringWithFormat:format,
+            theBytes.byte0, theBytes.byte1, theBytes.byte2, theBytes.byte3,
+            theBytes.byte4, theBytes.byte5, theBytes.byte6, theBytes.byte7,
+            theBytes.byte8, theBytes.byte9, theBytes.byte10, theBytes.byte11,
+            theBytes.byte12, theBytes.byte13, theBytes.byte14, theBytes.byte15];
+    
     CFRelease(theUUID);
-    return (__bridge NSString *) string;
+    
+    return uuid;
 }
 
 @end
